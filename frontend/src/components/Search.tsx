7cfe20ce1194753425a4517de00c8168c5879fb3
Pyrode <pyrode.01@gmail.com>
2026-07-28 19:11:02 +0530

    feat: Added toasts for search and update url for code

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+import { useToast } from "../context/toast"
 
 interface SearchProp {
     setCode: (code: number) => void
@@ -6,23 +8,38 @@ interface SearchProp {
 
 export const Search = ({ setCode }: SearchProp) => {
     const [codeText, setCodeText] = useState("")
+    const location = useLocation()
+    const { showToast } = useToast()
+    const navigate = useNavigate()
 
     const onClick = () => {
-        if (!/^\d{6}$/.test(codeText)) {
-            console.log("invalid code")
+        let text = codeText.trim()
+        navigate({
+            pathname: location.pathname,
+            search: `?code=${text}`
+        })
+    }
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const url_code = params.get("code")
+        if (url_code == null) {
             return
         }
-        setCode(+codeText)
-    }
+        if (!/^\d{6}$/.test(url_code)) {
+            showToast(`Invalid code: ${url_code}`, "error", 3000)
+            return
+        }
+        setCode(+url_code)
+    }, [location])
 
     return (
         <>
 
         <input
         type="search"
-        onChange={(e) => {
-            setCodeText(e.target.value)
-        }}
+        value={codeText}
+        onChange={(e) => setCodeText(e.target.value)}
         />
         <button onClick={onClick}>Download</button>
 
