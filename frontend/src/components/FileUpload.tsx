7ec80088d68452d006bba0cd62c8b780109674f4
Pyrode <pyrode.01@gmail.com>
2026-07-29 13:52:20 +0530

    feat: Using toast for upload status

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -2,6 +2,7 @@ import { type ChangeEvent, useState, useEffect } from "react";
 import { FileDisplay } from "./FileDisplay";
 import { createFileshare, uploadFile } from "../services/api";
 import { bytesToLargestUnit } from "../utils/utils";
+import { useToast } from "../context/toast";
 
 interface FileUploaderProps {
     onSizeChange: (size: number) => void
@@ -10,7 +11,7 @@ interface FileUploaderProps {
 export const FileUploader = ({ onSizeChange }: FileUploaderProps) => {
     const [files, setFiles] = useState<File[]>([]);
     const [code, setCode] = useState("");
-    const [status, setStatus] = useState("Ready")
+    const { showToast } = useToast()
 
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (!e.target.files) return;
@@ -38,6 +39,7 @@ export const FileUploader = ({ onSizeChange }: FileUploaderProps) => {
 
     const uploadFiles = async () => {
         if (files.length < 1) {
+            showToast("No files selected", "error", 3000)
             return
         }
         const tarballPromise: Promise<Uint8Array> = new Promise((res, rej) => {
@@ -52,23 +54,27 @@ export const FileUploader = ({ onSizeChange }: FileUploaderProps) => {
             }
             worker.postMessage({type: "create", data: files})
         })
-        setStatus("Processing")
+        showToast("Processing", "info", 3000)
         const done = await Promise.all([tarballPromise, createFileshare()])
 
         const data = done[0]
         const fileshare_info = done[1]
 
-        setStatus(`Uploading`)
+        showToast(`Uploading`, "info", 3000)
 
-        uploadFile(fileshare_info.url, data).then(() => setStatus("Ready"))
+        uploadFile(fileshare_info.url, data).then(() => {
+            showToast(`Uploaded ${bytesToLargestUnit(data.length)}`, "success", 3000)
+        })
         setFiles([])
-        setCode(`${fileshare_info.code} ${bytesToLargestUnit(data.length)}`)
+        setCode(`${fileshare_info.code}`)
     }
 
     return (
         <div>
-        <p>{status}</p>
-        {code != "" && <p>Uploaded to ID: {code}</p>}
+        {code != "" && 
+            <p>Uploaded with code: 
+            <a href={ `${window.location}download?code=${code}` }>{code}</a>
+        </p>}
 
         <input
         type="file"
