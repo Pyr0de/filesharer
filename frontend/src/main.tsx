4c8c349d34bf6b65a315851dbeb41c84dcc56d68
Pyrode <pyrode.01@gmail.com>
2026-07-29 19:41:13 +0530

    feat: Added new fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@fontsource/space-grotesk';
+import '@fontsource/inter';
 import App from './App.tsx'
 import { ToastProvider } from './context/toast.tsx'
 
