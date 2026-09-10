d354cac9f9cfb51141fc3d5b7e6c7caafcbffd3a
Pyrode <pyrode.01@gmail.com>
2026-09-10 13:16:51 +0530

    feat: 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { HomePage } from "./pages/Home";
 import { DownloadPage } from "./pages/Download";
 import { Header } from "./components/Header";
+import { NotFound } from "./pages/404";
 
 export default function App() {
     return (
@@ -10,7 +11,7 @@ export default function App() {
             <section className="flex h-[100vh] flex-col">
                 <Header />
                 <Routes>
-                    <Route path="/*" element={<p>404 Page not found</p>} />
+                    <Route path="/*" element={<NotFound />} />
                     <Route path="/" element={<HomePage />} />
                     <Route path="/download" element={<DownloadPage />} />
                 </Routes>
